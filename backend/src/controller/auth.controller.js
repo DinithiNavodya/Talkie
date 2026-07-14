6f2626bf020261682d388c30dad0794bbecb5b29
DinithiNavodya <dinithinavodyahh@gmail.com>
2026-07-15 00:11:25 +0530

    login refactoring

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -79,6 +79,10 @@ export const signup = async  (req, res) => {
 export const login = async (req, res) => {
     const { email, password } = req.body;
 
+    if(!email || !password){
+        return res.status(400).json({ message: 'Please provide both email and password' });
+    }
+
     try {
         const user = await User.findOne({ email });
         if (!user) {
