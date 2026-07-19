f297b60e2f028453835860546963621aad179b3b
DinithiNavodya <dinithinavodyahh@gmail.com>
2026-07-19 22:25:22 +0530

    implement the message input

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ const PORT = ENV.PORT || 3000;
 const __dirname = import.meta.dirname;
 
 
-app.use(express.json());
+app.use(express.json({limit: "5mb"}));
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -11,7 +11,7 @@ import ProfileHeader from "../components/ProfileHeader";
 function ChatPage() {
   const{activeTab,selectedUser} = useChatStore();
   return (
-    <div className='relative w-full max-w-6xl h-[800px]'>
+    <div className='relative w-full max-w-6xl h-[675px]'>
 
       <BorderAnimatedContainer>
         {/*Leftside*/}
